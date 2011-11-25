4aae3dac7f059cc6eb6b01fe2b20669fd2f0ce01
Antoni Batchelli <tbatchelli@acm.org>
2011-11-25 10:54:27 -0800

    Fix version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bridle "1.0.0-SNAPSHOT"
+(defproject bridle "0.0.1-SNAPSHOT"
   :description "Data structure constraints"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.match "0.2.0-alpha6"]]
